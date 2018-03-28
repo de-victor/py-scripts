8ac9d81261bf004364f074747dcf7fea1f7317a7
victor <2ndrunner@gmail.com>
2018-03-28 14:07:39 -0300

    modification on mine field game

--- mine.py
+++ mine.py
@@ -6,25 +6,66 @@ def constroi_tab(n,m):
         lista.append(constroi_linha(m))
     return lista
 
+def gerar_field(field, index):
+    bomba = gerar_bomba()
+    field['lista'].append(bomba)
+    field['indexes'].append(index)
+    field['qtd_bomba'] += 1 if bomba == '*' else 0
+
+    return field
 def constroi_linha(m):
-    lista = []
+    field = {'lista': [],
+             'indexes': [],
+             'qtd_bomba': 0}
+
     for _, index in enumerate(range(m)):
-        lista.append(gerar_bomba())
-    return lista
+        gerar_field(field, index)
+
+    return field
 
 def gerar_bomba():
     number = randint(0, 10000)
     return '*' if number %2 == 0 else '-'
 
-def print_tab(tab):
-    [print(x) for x in tab]
+def print_tab(tab, jogadas):
+    cont = 0
+    explodiu = False
+    while cont < len(tab) and not explodiu:
+        if len(jogadas) == 0:
+            for index, x in enumerate(tab[cont]['lista']):
+                print('[{}-{}]'.format(cont, index), end='')
+        else:
+            for index, x in enumerate(tab[cont]['lista']):
+                pos = [cont, index]
+                if pos in jogadas:
+                    if x == '*':
+                        explodiu = True
+                        break
+                    else:
+                        print('[ - ]'.format(cont, index), end='')
+                else:
+                    print('[{}-{}]'.format(cont, index), end='')
+        if explodiu:
+            print('Explodiu, fim de jogo')
+            break
+        else:
+            print()
+        cont += 1
+
+    return explodiu
 
 if __name__ == '__main__':
     tab = constroi_tab(*list(map(int, input('Informe o tamanho do tabuleiro(ex: 7 7): ').split(' '))))
-
+    jogadas = []
     jogando = True
     pos = [-1, -1]
+    print_tab(tab, jogadas)
 
     while jogando and pos[0] != 'sair':
-        print_tab(tab)
-        pos = input('Informe pos ou digite sair ').split(' ')
+        pos = input('Informe pos ou digite sair ').strip().split(' ')
+
+        if len(pos) == 2:
+            pos = list(map(int, pos))
+            jogadas.append(pos)
+            if print_tab(tab, jogadas):
+                break
